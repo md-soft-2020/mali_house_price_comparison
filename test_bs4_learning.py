911cecfd373012894e4fdaa77cf0a8b437fdced3
Mady Dembele <mady@fb.com>
2023-01-28 15:21:36 +0100

    learning purpose

---
+++ test_bs4_learning.py
@@ -0,0 +1,9 @@
+import requests as r
+from bs4 import BeautifulSoup as soup
+
+# url = "https://www.bamako-immobilier.com/Location_Appartements.html"
+# response = r.get(url)
+# doc = soup(response.text, "html.parser")
+
+# print(doc)
+
